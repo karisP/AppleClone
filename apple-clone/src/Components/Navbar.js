6399d8d0d4555f628942e00a9f691b37dbc1b718
Karissa <phillips.karissa00@gmail.com>
2021-04-29 20:38:43 -0400

    initial commit

---
+++ apple-clone/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import styles from './Navbar.module.css';
+console.log(styles);
+const Navbar = () => {
+    return(
+        <div className={styles.container}>
+            <div className={styles.hamburger}>1</div>
+            <div className={styles['apple-icon']}>2</div>
+            <div className={styles['cart-icon']}>3</div>
+        </div>
+    )
+}
+export default Navbar;
