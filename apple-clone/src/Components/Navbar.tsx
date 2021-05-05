ae7d58ef29df069ad290df532ad822cd9e72d4e5
Karissa <phillips.karissa00@gmail.com>
2021-05-04 21:32:56 -0400

    added hamburger to navbar, wip on tablet styles

--- apple-clone/src/Components/Navbar.tsx
+++ apple-clone/src/Components/Navbar.tsx
@@ -2,8 +2,21 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
     return(
         <div className={styles.container}>
-            <div className={styles.hamburger}></div>
+            <div className={styles.hamburger}>
+                <div></div>
+                <div></div>
+            </div>
             <div className={styles['apple-icon']}></div>
+            <div className={styles.tablet}>
+                <div>Mac</div>
+                <div>iPad</div>
+                <div>iPhone</div>
+                <div>Watch</div>
+                <div>TV</div>
+                <div>Music</div>
+                <div>Support</div>
+                <div className={styles.search}></div>
+            </div>
             <div className={styles['cart-icon']}></div>
         </div>
     )
